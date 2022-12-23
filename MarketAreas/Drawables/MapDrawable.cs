b36f05c9ff5310975e66a9d06beaa7443e8e57a7
Brock Dyer <bld4524@rit.edu>
2022-12-22 22:00:09 -0500

    Create basic UI layout.

---
+++ MarketAreas/Drawables/MapDrawable.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace MarketAreas.Drawables
+{
+	public class MapDrawable : IDrawable
+	{
+
+        public void Draw(ICanvas canvas, RectF dirtyRect)
+        {
+            canvas.StrokeColor = Colors.Purple;
+            canvas.StrokeSize = 4;
+            var width = dirtyRect.Right - dirtyRect.Left;
+            var height = dirtyRect.Bottom - dirtyRect.Top;
+            canvas.DrawRectangle(dirtyRect.Left, dirtyRect.Top, width, height);
+        }
+    }
+}
+
