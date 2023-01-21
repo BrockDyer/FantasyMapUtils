3c508ead8ff79b8a7403717e4dec782cd6f04f27
Brock Dyer <bld4524@rit.edu>
2023-01-20 19:30:13 -0500

    Implement beach line.

--- VoronoiModel.Tests/FortuneVoronoi/BeachLineTest.cs
+++ VoronoiModel.Tests/FortuneVoronoi/BeachLineTest.cs
@@ -10,37 +10,127 @@ public class BeachLineTest
     public void TestComputeBreakpoint()
     {
         // Setup
-        var pNeg1 = new Point2D(-1, 0);
-        var pOne = new Point2D(1, 0);
-        var pZero = new Point2D(0, -1);
-        var pPiOver3 = new Point2D(Math.Cos(Math.PI / 3), Math.Sin(Math.PI / 3));
+        var a = new Point2D(1, 0);
+        var b = new Point2D(3, 1);
+        const int y = 2;
 
-        var expectedCenter = new Point2D(0, 0);
-        var expectedPiOver3 = new Point2D(0.8543, 0.488);
-
-        const int sweepLineY = 1;
+        var leftExpected = new Point2D(1.83772233983, 0.824555320337);
+        var rightExpected = new Point2D(8.16227766017, -11.8245553203);
         
-        // Perform operation
-        var test1 = BeachLine.ComputeBreakpoint(pNeg1, pOne, sweepLineY);
-        var test2 = BeachLine.ComputeBreakpoint(pNeg1, pZero, sweepLineY);
-        var testHorizontal = BeachLine.ComputeBreakpoint(pOne, pNeg1, sweepLineY);
-        var test3 = BeachLine.ComputeBreakpoint(pOne, pPiOver3, sweepLineY);
+        // Perform
+        var leftBreakpoint = BeachLine.ComputeBreakpoint(a, b, y);
+        var rightBreakpoint = BeachLine.ComputeBreakpoint(b, a, y);
 
-        const string failureMessageTemplate = "Incorrect center for circle through {0} and {1} tangent to horizontal line at {2}";
+        bool PointsAreClose(Point2D p1, Point2D p2)
+        {
+            return Utils.AreClose(p1.X, p2.X) && Utils.AreClose(p1.Y, p2.Y);
+        }
+
+        // Assert
+        Assert.Multiple(() =>
+        {
+            Assert.That(PointsAreClose(leftBreakpoint, leftExpected), Is.True, $"Left breakpoint {leftBreakpoint} was not as expected {leftExpected}");
+            Assert.That(PointsAreClose(rightBreakpoint, rightExpected), Is.True, $"Right breakpoint {rightBreakpoint} was not as expected {rightExpected}");
+        });
+    }
+
+    private readonly Point2D _p1 = new(1, 0);
+    private readonly Point2D _p2 = new(3, 1);
+    private readonly Point2D _p3 = new(2, 2);
+
+    private BeachLine _beachLine = null!;
+
+    [SetUp]
+    public void Setup()
+    {
+        _beachLine = new BeachLine(_p1);
+    }
+
+    [Test]
+    public void TestInsertAndSplit()
+    {
+        // Perform
+        var initialEntry = new BeachLineEntry(0, _p1);
+        _beachLine.InsertAndSplit(initialEntry, _p2);
         
         // Assert
         Assert.Multiple(() =>
         {
-            Assert.That(test1, Is.EqualTo(expectedCenter), failureMessageTemplate, pNeg1, pOne, sweepLineY);
-            Assert.That(test2, Is.EqualTo(expectedCenter), failureMessageTemplate, pNeg1, pZero, sweepLineY);
-            const double tolerance = 0.1;
-            Assert.That(test3.X, Is.InRange(expectedPiOver3.X - tolerance, expectedPiOver3.X + tolerance), 
-                "Incorrect x center for circle passing through {0} and {1} tangent to horizontal line at {2}", pOne, 
-                pPiOver3, sweepLineY);
-            Assert.That(test3.Y, Is.InRange(expectedPiOver3.Y - tolerance, expectedPiOver3.Y + tolerance), 
-                "Incorrect y center for circle passing through {0} and {1} tangent to horizontal line at {2}", pOne, 
-                pPiOver3, sweepLineY);
-            Assert.That(testHorizontal, Is.EqualTo(expectedCenter), failureMessageTemplate, pOne, pNeg1, sweepLineY);
+            Assert.That(_beachLine, Has.Count.EqualTo(3), $"Incorrect count in beach line after adding {_p2}");
+            Assert.That(_beachLine.ToString(), Is.EqualTo($"<{_p1}, {_p2}, {_p1}>"), $"Beach line representation is incorrect after adding {_p2}");
+        });
+        
+        // Perform
+        var secondEntry = new BeachLineEntry(1, _p2);
+        _beachLine.InsertAndSplit(secondEntry, _p3);
+        
+        // Assert
+        Assert.Multiple(() =>
+        {
+            Assert.That(_beachLine, Has.Count.EqualTo(5), $"Incorrect count in beach line after adding {_p3}");
+            Assert.That(_beachLine.ToString(), Is.EqualTo($"<{_p1}, {_p2}, {_p3}, {_p2}, {_p1}>"), $"Incorrect representation after adding {_p3}");
+        });
+    }
+
+    [Test]
+    public void TestSearch()
+    {
+        // Perform
+        var result = _beachLine.Search(_p2.Y, _p2);
+        
+        // Assert
+        Assert.Multiple(() =>
+        {
+            Assert.That(result.Index, Is.EqualTo(0), "Result index is incorrect");
+            Assert.That(result.Site, Is.EqualTo(_p1), "Incorrect site in entry");
+        });
+        
+        // Setup
+        _beachLine.InsertAndSplit(new BeachLineEntry(0, _p1), _p2);
+        var test1 = new Point2D(1, 2); // Above p1
+        var test2 = new Point2D(1.8377, 2); // Above first breakpoint
+        var test3 = new Point2D(2.5, 2); // Above p2
+        var test4 = new Point2D(8.1623, 2); // Above second breakpoint
+        var test5 = new Point2D(9, 2); // Above p1 again
+
+        // Perform
+        var result1 = _beachLine.Search(test1.Y, test1);
+        var result2 = _beachLine.Search(test2.Y, test2);
+        var result3 = _beachLine.Search(test3.Y, test3);
+        var result4 = _beachLine.Search(test4.Y, test4);
+        var result5 = _beachLine.Search(test5.Y, test5);
+
+        void AssertResult(BeachLineEntry testResult, int testIndex, int expectedIndex, Point2D expectedSite)
+        {
+            Assert.That(testResult.Index, Is.EqualTo(expectedIndex), $"Result {testIndex} index was wrong");
+            Assert.That(testResult.Site, Is.EqualTo(expectedSite), $"Result {testIndex} site was wrong");
+        }
+        
+        // Assert
+        Assert.Multiple(() =>
+        {
+            AssertResult(result1, 1, 0, _p1);
+            AssertResult(result2, 2, 0, _p1);
+            AssertResult(result3, 3, 1, _p2);
+            AssertResult(result4, 4, 2, _p1);
+            AssertResult(result5, 5, 2, _p1);
+        });
+    }
+
+    [Test]
+    public void TestDelete()
+    {
+        // Setup
+        TestInsertAndSplit();
+        
+        // Perform
+        _beachLine.Delete(new BeachLineEntry(1, _p2));
+        
+        // Assert
+        Assert.Multiple(() =>
+        {
+            Assert.That(_beachLine, Has.Count.EqualTo(4));
+            Assert.That(_beachLine.ToString(), Is.EqualTo($"<{_p1}, {_p3}, {_p2}, {_p1}>"));
         });
     }
 }
--- VoronoiModel/FortuneVoronoi/BeachLine.cs
+++ VoronoiModel/FortuneVoronoi/BeachLine.cs
@@ -15,7 +15,17 @@ public class BeachLineEntry
 }
 public class BeachLine
 {
-    private string beachline;
+    private readonly List<Point2D> _beachLine;
+    
+    /// <summary>
+    /// Get the number of elements in the beach line.
+    /// </summary>
+    public int Count => _beachLine.Count;
+
+    public BeachLine(Point2D initialPoint)
+    {
+        _beachLine = new List<Point2D> { initialPoint };
+    }
 
     /// <summary>
     /// Compute the breakpoint between two points given the y coordinate of the sweep line. 
@@ -26,6 +36,11 @@ public class BeachLine
     /// <returns>The center of the circle tangent to the sweep line that passes through p1 and p2.</returns>
     public static Point2D ComputeBreakpoint(Point2D p1, Point2D p2, double y)
     {
+        // https://www.desmos.com/calculator/fsp6oy3lh0
+        // Find the intersection between the perpendicular bisector of p1 and p2, and the parabola defined by
+        // p1 as a focus and y as a directrix.
+        // Special thanks to Richard Catalano Jr. for helping me through the geometry of the problem.
+        
         var ax = p1.X;
         var ay = p1.Y;
         var bx = p2.X;
@@ -51,23 +66,10 @@ public class BeachLine
         var c = Math.Pow(ax, 2) + Math.Pow(ay, 2) - Math.Pow(y, 2) - 2 * (ay - y) * (py - mb * px);
 
         var sqrtPart = Math.Sqrt(Math.Pow(b, 2) - 4 * c);
-        var cx1 = (-b + sqrtPart) / 2;
-        var cx2 = (-b - sqrtPart) / 2;
+        var cxRight = (-b + sqrtPart) / 2;
+        var cxLeft = (-b - sqrtPart) / 2;
 
-        var leftBound = Math.Min(ax, bx);
-        var rightBound = Math.Max(ax, bx);
-
-        if (cx1 >= leftBound && cx1 <= rightBound)
-        {
-            return new Point2D(cx1, ComputeCy(cx1));
-        }
-
-        if (cx2 >= leftBound && cx2 <= rightBound)
-        {
-            return new Point2D(cx2, ComputeCy(cx2));
-        }
-
-        throw new InvalidOperationException($"No solution for {p1} and {p2} with sweep line at {y}");
+        return p1.X < p2.X ? new Point2D(cxLeft, ComputeCy(cxLeft)) : new Point2D(cxRight, ComputeCy(cxRight));
     }
     
     /// <summary>
@@ -78,7 +80,42 @@ public class BeachLine
     /// <returns>The entry in the beach line that is directly above the new site.</returns>
     public BeachLineEntry Search(double y, Point2D newSite)
     {
-        throw new NotImplementedException();
+        var start = 0;
+        var end = _beachLine.Count - 1;
+
+        while (true)
+        {
+            var middle = (int)Math.Round((start + end) / 2d);
+            
+            if (middle == start || middle == end)
+            {
+                return new BeachLineEntry(middle, _beachLine[middle]);
+            }
+            
+            var middleArc = _beachLine[middle];
+
+            var leftArc = _beachLine[middle - 1];
+            var rightArc = _beachLine[middle + 1];
+
+            var leftBreakpoint = ComputeBreakpoint(leftArc, middleArc, y);
+            var rightBreakpoint = ComputeBreakpoint(middleArc, rightArc, y);
+
+            // If the new site is between the two breakpoints, it is under the current arc.
+            if (leftBreakpoint.X <= newSite.X && rightBreakpoint.X >= newSite.X)
+            {
+                return new BeachLineEntry(middle, middleArc);
+            }
+
+            // Determine which way to search next.
+            if (newSite.X < leftBreakpoint.X)
+            {
+                end = middle;
+            }
+            else
+            {
+                start = middle;
+            }
+        }
     }
 
     /// <summary>
@@ -87,9 +124,12 @@ public class BeachLine
     /// <param name="existingArc">The existing arc to split.</param>
     /// <param name="newArc">The new arc to add to the beach line.</param>
     /// <returns>The new entry in the beach line.</returns>
-    public BeachLineEntry InsertAndSplit(Point2D existingArc, Point2D newArc)
+    public BeachLineEntry InsertAndSplit(BeachLineEntry existingArc, Point2D newArc)
     {
-        throw new NotImplementedException();
+        _beachLine.Insert(existingArc.Index + 1, newArc);
+        _beachLine.Insert(existingArc.Index + 2, existingArc.Site);
+
+        return new BeachLineEntry(existingArc.Index + 1, newArc);
     }
 
     /// <summary>
@@ -98,6 +138,11 @@ public class BeachLine
     /// <param name="entry">The entry to delete.</param>
     public void Delete(BeachLineEntry entry)
     {
-        throw new NotImplementedException();
+        _beachLine.RemoveAt(entry.Index);
+    }
+
+    public override string ToString()
+    {
+        return "<" + string.Join(", ", _beachLine) + ">";
     }
 }
